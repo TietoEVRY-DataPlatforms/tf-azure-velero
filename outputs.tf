c2df610be551eda5adc27a3099c76d5960f3a12a
Carsten Thiel <carsten.thiel@tietoevry.com>
2024-01-05 10:11:09 +0100

    Adding all metadata.json fields to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
-output "velero_client_id" {
-  value = azurerm_user_assigned_identity.backup_identity.client_id
+output "azure_backup" {
+  description = "Values to be included in the metadata.json"
+  value = {
+    client_id          = azurerm_user_assigned_identity.backup_identity.client_id
+    nodes_rg           = var.node_resource_group
+    resource_id        = azurerm_user_assigned_identity.backup_identity.id
+    storage_account    = azurerm_storage_account.aks_backup.name
+    storage_account_rg = azurerm_resource_group.aks_backup.name
+    storage_region     = azurerm_resource_group.aks_backup.location
+    subscription_id    = var.subscription_id
+  }
 }
-
